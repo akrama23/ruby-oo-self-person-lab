96db6944e83f15b7756e48796420ade4fc635492
Andres-R-Hernandez <andres.hernandez@utexas.edu>
2020-11-18 15:45:12 -0600

    added more methods, andres

--- lib/person.rb
+++ lib/person.rb
@@ -1,16 +1,34 @@
 # your code goes here
 class Person
 
-    attr_accessor :bank_account, :happiness, :hygiene
+    attr_accessor :bank_account, :friend
 
-    attr_reader :name
+    attr_reader :name, :happiness, :hygiene
 
     def initialize(name)
             @name = name
             @bank_account = 25
             @happiness = 8
             @hygiene = 8
-    end 
+    end
+
+    def happiness=(points)
+      @happiness += points
+      if @happiness > 10
+        @happiness = 10
+      elsif @happiness < 0
+        @happiness = 0
+      end
+    end
+
+    def hygiene=(points)
+      @hygiene += points
+      if @hygiene > 10
+        @hygiene = 10
+      elsif @hygiene < 0
+        @hygiene = 0
+      end
+    end
 
     def clean?
         @hygiene > 7
@@ -23,14 +41,43 @@ class Person
     def get_paid(salary)
         @bank_account += salary
          "all about the benjamins"
-    end 
+    end
 
     def take_bath
-        @hygiene += 4 
-        if @hygiene > 10 
-            @hygiene = 10
-        end 
+        @hygiene += 4
+
         "♪ Rub-a-dub just relaxing in the tub ♫"
     end
-    
+
+    def work_out
+      @happiness += 2
+      @hygiene -= 3
+      "♪ another one bites the dust ♫"
+    end
+
+    def call_friend(friend)
+#      self.friend = friend
+
+
+    end
+
+    def start_conversation(conversation_starter, topic)
+
+#      self.conversation_starter = friends
+
+      if topic == "politics"
+#        self.@happiness -= 1
+
+        "blah blah partisan blah lobbyist"
+      elsif topic == "weather"
+        "blah blah sun blah rain"
+#        self.@happiness += 1
+      else
+        "blah blah blah blah blah"
+      end
+
+
+    end
+
+
 end
